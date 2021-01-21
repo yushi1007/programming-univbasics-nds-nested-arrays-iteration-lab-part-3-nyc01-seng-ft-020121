522bb95772425e7de4ed3fbddf66da894d170058
Yu Shi <yushiys95@gmail.com>
2021-01-21 04:51:21 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,14 +1,17 @@
 def join_nested_strings(src)
   # src will be an Array of Arrays of Strings and Integers
   # Combine all Strings present in the AoA into a single value and return it
-  total = 0
+outer_results = []
 row_index = 0
-while row_index < guessing_game_grid.count do
+while row_index < spice_rack.count do
   element_index = 0
-  while element_index < guessing_game_grid[row_index].count do
-    total += guessing_game_grid[row_index][element_index]
+  longest_string_element = ""
+  while element_index < spice_rack[row_index].count do
+    if spice_rack[row_index][element_index].length > longest_string_element.length
+      longest_string_element = spice_rack[row_index][element_index]
+    end
     element_index += 1
   end
+  outer_results << longest_string_element
   row_index += 1
-end
 end
